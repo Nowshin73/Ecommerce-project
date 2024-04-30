3d32dc6abc6657f25089e79fe562e8e9913bdeb3
Nowshin Nawar <68994761+Nowshin73@users.noreply.github.com>
2024-04-30 18:17:20 +0600

    backend working

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -5,4 +5,5 @@ const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/products/new").post(createProduct);
+router.route("/products/:id").put();
 module.exports = router;
