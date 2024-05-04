0dbe85601aa4c5aaea503caff775c1677cc5da1b
Nowshin Nawar <68994761+Nowshin73@users.noreply.github.com>
2024-05-04 22:28:38 +0600

    handling uncaught exception

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDB = require("./config/database");
+const { ConnectionStates } = require("mongoose");
 dotenv.config({path:"backend/config/config.env"});
 connectDB();
+
+process.on("uncaughtException",(err)=>{
+    console.log(`Error:${err.message}`);
+    console.log(" shutting the down the server due to uncaught Exception");
+    process.exit(1);
+})
 const server = app.listen(process.env.PORT,()=>{
     console.log(`the http://localhost:${process.env.PORT} has been connected`);
 })
