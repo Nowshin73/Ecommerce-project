b4490b80365a5727ba9a47367a57dc50f747462a
Nowshin Nawar <68994761+Nowshin73@users.noreply.github.com>
2024-05-04 21:57:13 +0600

    error corrected

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const connectDB = ()=>{
-    mongoose.connect(process.env.DB_URI).then((data)=>{
+    mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true}).then((data)=>{
         console.log(`Mongodb connected with server: ${data.connection.host}`);
     }).catch((err)=>{
         console.log(err);
