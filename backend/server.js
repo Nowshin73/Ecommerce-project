d50bbbe265e17a56bce97051b57bf1c9501143b4
Nowshin Nawar <68994761+Nowshin73@users.noreply.github.com>
2024-05-04 22:22:28 +0600

    handled unwanted rejection

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,14 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/database");
 dotenv.config({path:"backend/config/config.env"});
 connectDB();
-app.listen(process.env.PORT,()=>{
+const server = app.listen(process.env.PORT,()=>{
     console.log(`the http://localhost:${process.env.PORT} has been connected`);
+})
+
+process.on("unhandledRejection", (err)=>{
+    console.log(`error: ${err.message}`);
+    console.log(" shutting the down the server due to unhandled rejection");
+    server.close(()=>{
+        process.exit(1);
+    })
 })
